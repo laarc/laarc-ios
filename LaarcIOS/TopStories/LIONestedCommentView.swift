7ae993001dd88b4466025ddcbd0702a92ce3b0a6
Emily Kolar <emkolardev@gmail.com>
2019-01-22 13:28:35 -0600

    start user view; increments build styff

---
+++ LaarcIOS/TopStories/LIONestedCommentView.swift
@@ -0,0 +1,21 @@
+//
+//  LIONestedCommentView.swift
+//  LaarcIOS
+//
+//  Created by Emily Kolar on 1/21/19.
+//  Copyright © 2019 Emily Kolar. All rights reserved.
+//
+
+import UIKit
+
+class LIONestedCommentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ LaarcIOS/User/LIOUserView.swift
@@ -0,0 +1,21 @@
+//
+//  LIOUserView.swift
+//  LaarcIOS
+//
+//  Created by Emily Kolar on 1/19/19.
+//  Copyright © 2019 Emily Kolar. All rights reserved.
+//
+
+import UIKit
+
+class LIOUserView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
